1524b8c2c200cd49ee1b275623860e462b0f5d38
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2020-07-18 11:07:22 +0300

    1. Загрузка данных в вид[#309508]

--- src/main/java/ru/job4j/accident/control/IndexControl.java
+++ src/main/java/ru/job4j/accident/control/IndexControl.java
@@ -4,10 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class IndexControl {
     @GetMapping("/")
     public String index(Model model) {
+        List<String> list = List.of(
+                "Petr Arsentev", "Stas Korobeynikov", "Elon Musk", "Bill Gates"
+        );
+        model.addAttribute("strings", list);
         return "index";
     }
 }
