0bcb517ff6d32817c5fcc22ce340d6ed3cce88a4
staskorobeynikov <stas.korobeinikov@mail.ru>
2021-06-22 20:10:12 +0300

    add test for LoginControl

---
+++ src/test/java/ru/job4j/accident/control/LoginControlTest.java
@@ -0,0 +1,56 @@
+package ru.job4j.accident.control;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LoginControlTest {
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+
+        this.mockMvc = MockMvcBuilders.standaloneSetup(new LoginControl())
+                .setViewResolvers(viewResolver)
+                .build();
+    }
+
+    @Test
+    public void whenRedirectLoginJspWithIncorrectPassword() throws Exception {
+        this.mockMvc.perform(get("/login?error=true"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"))
+                .andExpect(model().attribute("errorMessage", "Username or Password is incorrect !!"));
+    }
+
+    @Test
+    public void whenRedirectLoginJspWithErrorLogout() throws Exception {
+        this.mockMvc.perform(get("/login?logout=true"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"))
+                .andExpect(model().attribute("errorMessage", "You have been successfully logged out !!"));
+    }
+
+    @Test
+    public void whenRedirectLoginJspWithLogoutRequest() throws Exception {
+        this.mockMvc.perform(get("/logout"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login?logout=true"));
+    }
+}
